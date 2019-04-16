c5ce8519b5b1c3e4487ea4c900c891a24f9f23a2
Jacob Lutin <jacobl1933@gmail.com>
2019-04-16 15:36:19 +0300

    establish connection with google sheet

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import config from './config';
+
+const url = `https://sheets.googleapis.com/v4/spreadsheets/${ config.spreadsheetId }/values:batchGet?ranges=Sheet1&majorDimension=ROWS&key=${ config.apiKey }`;
+
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+     items:[]
+    };
+  }
+
+  componentDidMount() {
+    fetch(url).then(response => response.json()).then(data => {
+      let batchRowValues = data.valueRanges[0].values;
+
+      const rows = [];
+      for (let i = 1; i < batchRowValues.length; i++) {
+        let rowObject = {};
+        for (let j = 0; j < batchRowValues[++i].length; j++) {
+          rowObject[batchRowValues[0][j]] = batchRowValues[i][j];
+        }
+        rows.push(rowObject);
+      }
+
+      this.setState({ items: rows });
+    });
+  }
+
   render() {
     return (
       <div className="App">
---
+++ src/config.js
@@ -0,0 +1,4 @@
+export default {
+  apiKey: 'AIzaSyCFJOPwyO9_vOSZVu5A5M8E9KzP8-efX0o',
+  spreadsheetId: '1sCgmzBIq2K9jUckLuYSWbDq4CuNUfdtuE6a5xI3I5Hw'
+}
